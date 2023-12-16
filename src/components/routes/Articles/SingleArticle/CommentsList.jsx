8aed8a5cbde10b7d993f2c056d0477360e1ddc0e
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2023-12-16 00:14:22 +0000

    add confirmation and failure messages to delete comment function

--- src/components/routes/Articles/SingleArticle/CommentsList.jsx
+++ src/components/routes/Articles/SingleArticle/CommentsList.jsx
@@ -4,6 +4,7 @@ import { getArticleComments } from '../../../../../api';
 import PostComment from './PostComment';
 import DeleteButton from './DeleteButton';
 import { SpinnerLoader } from '../../../base/SpinnerLoader';
+import moment from 'moment/moment';
 
 function CommentsList({ article_id }) {
     const [comments, setComments] = useState([]);
@@ -12,6 +13,8 @@ function CommentsList({ article_id }) {
     const [isLoading, setIsLoading] = useState(true);
     const [showSuccessMessage, setShowSuccessMessage] = useState(false);
     const [showFailureMessage, setShowFailureMessage] = useState(false);
+    const [showDeleteSuccess, setShowDeleteSuccess] = useState(false);
+    const [showDeleteFailure, setShowDeleteFailure] = useState(false);
 
     const loadComments = () => {
         setIsLoading(true);
@@ -45,15 +48,28 @@ function CommentsList({ article_id }) {
         loadComments();
     }, [article_id]);
 
-    const handleDeleteComment = (commentId) => {
+    const handleDeleteComment = (commentId, status) => {
+        if (status === false){
+            setShowDeleteFailure(true);
+            
+            setTimeout(() => {
+                setShowDeleteFailure(false);
+            }, 4000);
+        } else {
+            setShowDeleteSuccess(true);
         setComments(comments.filter(comment => comment.comment_id !== commentId));
+            setTimeout(() => {
+                setShowDeleteSuccess(false);
+            }, 2000);
+        }
     };
-    
 
     return (
         <div className='comments-container'>
             {showSuccessMessage && <p className="success-message">Comment posted successfully!</p>}
             {showFailureMessage && <p className="failure-message">Failed to post comment. Please try again later.</p>}
+            {showDeleteSuccess && <p className="success-message">Comment deleted successfully!</p>}
+            {showDeleteFailure && <p className="failure-message">Failed to delete comment. Please try again later.</p>}
 
             <div className="comments-section">
                 <h2>Comments ({comments?.length ?? 0})</h2>
@@ -79,13 +95,10 @@ function CommentsList({ article_id }) {
                             </div>
                         </div>
                     ))
-                ) : (
-                    !isLoading && <p>No comments yet. Be the first to comment!</p>
-                )}
+                ) : (!isLoading && <p>No comments yet. Be the first to comment!</p>)}
             </div>
         </div>
     );
-
 }
 
 export default CommentsList;
